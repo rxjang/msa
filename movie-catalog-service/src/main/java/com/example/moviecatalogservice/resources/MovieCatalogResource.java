0a953fdfdf48f43d858d2f44c7513063ee42655d
Seohyun Jang <olllehyun@gmail.com>
2022-07-12 17:23:40 +0900

    add Apis for movie catalog

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,14 +3,16 @@ package com.example.moviecatalogservice.resources;
 import com.example.moviecatalogservice.models.CatalogItem;
 import com.example.moviecatalogservice.models.Movie;
 import com.example.moviecatalogservice.models.Rating;
+import com.example.moviecatalogservice.models.UserRating;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.lang.reflect.ParameterizedType;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,20 +20,28 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    private final RestTemplate restTemplate;
+    private final WebClient.Builder webClientBuilder;
+
+    public MovieCatalogResource(RestTemplate restTemplate, WebClient.Builder webClientBuilder) {
+        this.restTemplate = restTemplate;
+        this.webClientBuilder = webClientBuilder;
+    }
+
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 
-        RestTemplate restTemplate = new RestTemplate();
+        UserRating ratings = restTemplate.getForObject("http://localhost:8083/ratingsdata/users/" + userId, UserRating.class);
 
-        // get all rated movie ids
-        List<Rating> ratings = Arrays.asList(
-                new Rating("1234", 4),
-                new Rating("5678", 3)
-        );
-        // for each movie ID, call movie info service and get details
-        // put them all together
-        return ratings.stream().map(rating -> {
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+        return ratings.getUserRating().stream().map(rating -> {
+            // for each movie ID, call movie info service and get details
+            Movie movie = webClientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                    .retrieve()
+                    .bodyToMono(Movie.class)
+                    .block();
+//             put them all together
             return new CatalogItem(movie.getName(), "Description", rating.getRating());
         }).collect(Collectors.toList());
     }
--- rating-data-service/src/main/java/com/example/ratingdataservice/resources/RatingResources.java
+++ rating-data-service/src/main/java/com/example/ratingdataservice/resources/RatingResources.java
@@ -1,10 +1,14 @@
 package com.example.ratingdataservice.resources;
 
 import com.example.ratingdataservice.models.Rating;
+import com.example.ratingdataservice.models.UserRating;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/ratingsdata")
@@ -14,5 +18,18 @@ public class RatingResources {
     public Rating getRatings(@PathVariable("movieId") String movieId) {
 
         return new Rating(movieId, 4);
+    }
+
+    @GetMapping("/users/{userId}")
+    public UserRating getUserRatings(@PathVariable("userId") String userId) {
+
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("5678", 3)
+        );
+
+        UserRating userRating =  new UserRating();
+        userRating.setUserRating(ratings);
+        return userRating;
     }
 }
